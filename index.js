2c3a559d9c16c195dbb787346fba9ef5b741588b
Dmitry Zhuk <zhuchara@gmail.com>
2017-07-19 16:52:28 +0300

    standby added to _handleMessage

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ class Bot extends EventEmitter {
     let entries = json.entry
 
     entries.forEach((entry) => {
-      let events = entry.messaging
+      let events = entry.messaging || entry.standby
 
       events.forEach((event) => {
         // handle inbound messages and echos
